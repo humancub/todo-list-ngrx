d2ac429a30709c4495db92f13324bea315f73571
Olga Murashko <olgamurashko@Olgas-MacBook-Pro.local>
2021-04-26 18:15:52 +0300

    refactored from subscription to async pipe

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { FormControlDirective } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ITodo } from '../models/todo.interface';
@@ -10,10 +9,9 @@ import {
   ToggleTheme,
   UpdateTodo,
 } from '../store/actions';
-import { IAppState, ITodosState } from '../store/reducers';
+import { IAppState } from '../store/reducers';
 import { currentTheme, loading, todos } from '../store/selectors';
 import { FormControl } from '@angular/forms';
-import { IThemeState } from '../store/reducers/theme.reducer';
 import { Theme } from '../core/enums/theme.enum';
 
 @Component({
@@ -24,14 +22,18 @@ import { Theme } from '../core/enums/theme.enum';
 export class ToDoListComponent implements OnInit {
   public todos$: Observable<ITodo[]> = this.store.select(todos);
   public loading$: Observable<boolean> = this.store.select(loading);
+  public theme$: Observable<Theme> = this.store.select(currentTheme);
 
   public newTodo = new FormControl('');
   public editedTodo = new FormControl('');
+
   public selectedTodo: ITodo | null = null;
-  public editedMode = false;
-  public theme: Theme | undefined;
 
   constructor(private store: Store<IAppState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(GetTodos());
+  }
 
   removeTodo(todo: ITodo) {
     this.store.dispatch(RemoveTodo({ todo }));
@@ -44,6 +46,7 @@ export class ToDoListComponent implements OnInit {
 
   updateTodo(id: string, changedTitle: string): void {
     this.store.dispatch(UpdateTodo({ todo: { id, title: changedTitle } }));
+    this.selectedTodo = null;
   }
 
   setTodo(todo: ITodo): void {
@@ -53,13 +56,5 @@ export class ToDoListComponent implements OnInit {
 
   public toggleTheme() {
     this.store.dispatch(ToggleTheme());
-  }
-
-  ngOnInit(): void {
-    this.store.dispatch(GetTodos());
-
-    this.store.select(currentTheme).subscribe((theme: Theme) => {
-      this.theme = theme;
-    });
   }
 }
